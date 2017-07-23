b2d6e4bbadc974ca458a8d4979da0666307a1151
Fumio Iwai <gustav-mahler@future.ocn.ne.jp>
2017-07-23 23:23:56 +0900

    first commit.

---
+++ CreateRaw/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CreateRaw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFileNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(92, 20);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 25);
+            this.textBoxWidth.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height";
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(92, 72);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 25);
+            this.textBoxHeight.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(54, 173);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(96, 34);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FilleNum";
+            // 
+            // textBoxFileNum
+            // 
+            this.textBoxFileNum.Location = new System.Drawing.Point(93, 122);
+            this.textBoxFileNum.Name = "textBoxFileNum";
+            this.textBoxFileNum.Size = new System.Drawing.Size(100, 25);
+            this.textBoxFileNum.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 219);
+            this.Controls.Add(this.textBoxFileNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxWidth);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFileNum;
+    }
+}
+
---
+++ CreateRaw/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateRaw
+{
+    public partial class Form1 : Form
+    {
+        string mPrefix = "data";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Text = Application.ProductName;
+
+            this.textBoxWidth.Text  = "1";
+            this.textBoxHeight.Text = "1";
+
+            this.textBoxFileNum.Text = "10";
+        }
+
+        //---------------------------------------------------------------------
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            int imgW = int.Parse(this.textBoxWidth.Text );
+            int imgH = int.Parse(this.textBoxHeight.Text);
+
+            int imgWH = imgW * imgH;
+
+            ushort[] data = new ushort[imgWH];
+
+            int NumOfFiles = int.Parse(this.textBoxFileNum.Text);
+
+            for (int nFile = 0; nFile < NumOfFiles; nFile++)
+            {
+                for (int n = 0; n < imgWH; n++)
+                {
+                    data[n] = (ushort)(n + nFile * imgW / NumOfFiles);
+                }
+
+                string filename = this.mPrefix + "_W" + imgW.ToString("0000") + "_H" + imgH.ToString("0000") + "_N" + nFile.ToString("0000") + ".raw";
+                System.IO.BinaryWriter bw = new System.IO.BinaryWriter(new System.IO.FileStream(filename, System.IO.FileMode.OpenOrCreate));
+
+                for (int n = 0; n < imgWH; n++)
+                {
+                    bw.Write(data[n]);
+                }
+
+                bw.Close();
+            }
+
+            MessageBox.Show("Finished");
+        }
+    }
+}
